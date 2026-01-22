b6d0796df7e12262a8e53ccf900e651519bcd737
lukasschirren <l.schirren@web.de>
2026-01-22 19:38:33 +0000

    Scaling Ukrainian electricity demand

--- scripts/build_electricity_demand.py
+++ scripts/build_electricity_demand.py
@@ -265,6 +265,9 @@ if __name__ == "__main__":
         # hack indices (currently, UA is manually set to 2018)
         load_ua.index -= time_diff
         load["UA"] = load_ua
+        # # Scale Ukraine demand from 154.81 TWh (2018) to 105.208 TWh (IEA - electricity production)
+        # ua_scaling_factor = 105.208 / 154.81  # Approximately 0.68
+        # load["UA"] = load_ua * ua_scaling_factor
         # attach load of MD (no time-series available, use 2020-totals and distribute according to UA):
         # https://www.iea.org/data-and-statistics/data-browser/?country=MOLDOVA&fuel=Energy%20consumption&indicator=TotElecCons
         if "MD" in countries:
